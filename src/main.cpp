7726b867c1636230e24280a70abb9e3a2b45e593
Lukas Kirner <lkirner@inovex.de>
2023-03-08 14:55:31 +0100

    Enable Shell over USB

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,23 @@
 
 #include "app_task.h"
 
+#include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/usb/usb_device.h>
 
-LOG_MODULE_REGISTER(app, CONFIG_MATTER_LOG_LEVEL);
+LOG_MODULE_REGISTER(app, CONFIG_MATTER_LOG_LEVEL_DBG);
 
 int main() {
-	CHIP_ERROR err = AppTask::Instance().StartApp();
+	CHIP_ERROR err = CHIP_NO_ERROR;
+
+	err = chip::System::MapErrorZephyr(usb_enable(NULL));
+	if (err != CHIP_NO_ERROR) {
+		LOG_ERR("Failed to initialize USB device");
+		return EXIT_FAILURE;
+	}
+
+	LOG_DBG("Starting App...");
+	err = AppTask::Instance().StartApp();
 
 	LOG_ERR("Exited with code %" CHIP_ERROR_FORMAT, err.Format());
 	return err == CHIP_NO_ERROR ? EXIT_SUCCESS : EXIT_FAILURE;
